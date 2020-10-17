2896948374606e2434d9d7de7311ee4dcee01af3
ricbaro <70449536+ricbaro@users.noreply.github.com>
2020-10-17 11:37:07 -0700

    Update basic calculator.py

--- basic calculator.py
+++ basic calculator.py
@@ -1,129 +1,163 @@
-# try:
-#     print("Division Calculator")
-#     nums = []
-#     nums.append(int(input("Enter the first integer: ")))
-#     nums.append(int(input("Enter the second integer: ")))
-#     #nums.append(int(nums[0]/nums[1]))
-#     print("{0} / {1} = {2}".format(nums[0],nums[1],nums[2]))
-# except ValueError:
-#     print("Invalid input!")
-# except ZeroDivisionError as error:
-#     print(error)vv
-# except Exception as error: 
-#     print(error)
+# now only first index is the answer need to change
+# play again menu
+# image?
+# in/correct ui
+# division deciaml point 
+# atleast second trial
 
 from tkinter import *
+from random import *
+# from time import *
+# import threading
 
 win = Tk()
-win.title("Simple Calculator!")
+win.geometry("350x450")
 
-# win.geometry("1000x500")
-e = Entry(win, width = 35, borderwidth = 5)
-e.grid(row = 0, column = 0, columnspan = 3, padx = 10, pady = 10)
+Label(win,text = "ASMD",justify = CENTER,font = "Times 21").pack()
+Label(win,text = "Learning Center",font = "Times 17").pack()
 
-def button_click(number):
-    # e.delete(0,END)
-    current = e.get()
-    e.delete(0,END)
+frame = Frame(win)
 
-    e.insert(0, current + str(number))
+# getting x , y
+def click():
+    num1 = int(random()*10+1)
+    num2 = int(random()*10+1)
+    return num1,num2
 
-def button_clear():
-    e.delete(0,END)
+def destroyMenu():
+    btn_addition.destroy()
+    btn_subtraction.destroy()
+    btn_multiplication.destroy()
+    btn_dvision.destroy()
+    btn_exit.destroy()
+# generating buttons for ans
+def ansButton(anslist):
+    bt1 = Button(frame,text= str(anslist[0]),padx = 35,pady = 35,command = lambda: checkAns(anslist[0]))
+    bt2 = Button(frame,text= str(anslist[1]),padx = 35,pady = 35,command = lambda: checkAns(anslist[1]))
+    bt3 = Button(frame,text= str(anslist[2]),padx = 35,pady = 35,command = lambda: checkAns(anslist[2]))
+    bt4 = Button(frame,text= str(anslist[3]),padx = 35,pady = 35,command = lambda: checkAns(anslist[3]))
+   
+    bt1.grid(row=1,column=0)
+    bt2.grid(row=1,column=1)
+    bt3.grid(row=2,column=0)
+    bt4.grid(row=2,column=1)
+    
+    def destroy():
+        bt1.destroy()
+        bt2.destroy()
+        bt3.destroy()
+        bt4.destroy()
+#check the answer 
+    def checkAns(ans):
+        destroy()
+        if ans == realAns:
+            ansLabel = Label(frame,text="CORRECT!",font = "Time 21")
+            ansLabel.grid(row=1,column=0)
+        else:
+            ansLabel = Label(frame,text="Wrong!",font = "Time 21")
+            ansLabel.grid(row=1,column=0)
+        
 
-def button_add():
-    f_num = e.get()
-    global first_number
-    global arithmetic
-    arithmetic = "+"
-    first_number = int(f_num)
-    e.delete(0,END)
-
-
-def button_subtraction():
-    f_num = e.get()
-    global first_number
-    global arithmetic
-    arithmetic = "-"
-    first_number = int(f_num)
-    e.delete(0,END)
-
-def button_multiplication():
-    f_num = e.get()
-    global first_number
-    global arithmetic
-    arithmetic = "x"
-    first_number = int(f_num)
-    e.delete(0,END)
-
-
-def button_equal():
-    s_num = e.get()
-    e.delete(0,END)
-    ans_promt = "{0} {1} {2} = ".format(first_number,arithmetic,s_num)
-    # ans = first_number + int(s_num)
-    # e.insert(0,ans_promt + str(ans))
-    #  + " + " + s_num + " = " + (first_number + int(s_num)))
-    # e.insert(0,first_number + int(s_num))
-
+# store the followings into list
+def storeAns(x,y):
+    # print("inside storeAns")
+    # print("arithmetic: " + arithmetic)
+    anslist = []
+    global realAns
     if arithmetic == "+":
-        ans = first_number + int(s_num)
-        e.insert(0,ans_promt + str(ans))
+        realAns = x+y
+        anslist.append(realAns)
+        for x in range(1,4):
+            ans = int(random()*10)
+            anslist.append(ans)
     elif arithmetic == "-":
-        ans = first_number - int(s_num)
-        e.insert(0,ans_promt + str(ans))
+        realAns = x-y
+        anslist.append(realAns)
+        for x in range(1,4):
+            ans = int(random()*10)
+            anslist.append(ans)
     elif arithmetic == "x":
-        ans = first_number * int(s_num)
-        e.insert(0,ans_promt + str(ans))
-    else:
-        e.insert(0,"Invalid")
+        realAns = x*y
+        anslist.append(realAns)
+        for x in range(1,4):
+            ans = int(random()*10)
+            anslist.append(ans)
+    elif arithmetic == "/":
+        realAns = x/y
+        anslist.append(realAns)
+        for x in range(1,4):
+            ans = int(random()*10)
+            anslist.append(ans)
+    return anslist
+
     
 
-button_1 = Button(win, text = "1", padx = 35, pady = 21, command = lambda: button_click(1))
-button_2 = Button(win, text = "2", padx = 35, pady = 21, command = lambda: button_click(2))
-button_3 = Button(win, text = "3", padx = 35, pady = 21, command = lambda: button_click(3))
-button_4 = Button(win, text = "4", padx = 35, pady = 21, command = lambda: button_click(4))
-button_5 = Button(win, text = "5", padx = 35, pady = 21, command = lambda: button_click(5))
-button_6 = Button(win, text = "6", padx = 35, pady = 21, command = lambda: button_click(6))
-button_7 = Button(win, text = "7", padx = 35, pady = 21, command = lambda: button_click(7))
-button_8 = Button(win, text = "8", padx = 35, pady = 21, command = lambda: button_click(8))
-button_9 = Button(win, text = "9", padx = 35, pady = 21, command = lambda: button_click(9))
-button_0 = Button(win, text = "0", padx = 35, pady = 21, command = lambda: button_click(0))
-
-
-button_add = Button(win, text = "+", padx = 45, pady = 21, command = button_add)
-button_subtraction = Button(win, text = "-", padx = 45, pady = 21, command = button_subtraction)
-button_multiplication = Button(win, text = "x", padx = 45, pady = 21, command = button_multiplication)
-# button_backspace = Button(win, text = "<--", padx = 35, pady = 21, command = button_multiplication)
-
-button_equal = Button(win, text = "=", padx = 77, pady = 21, command = button_equal)
-button_clear = Button(win, text = "Clear", padx = 35, pady = 21, command = button_clear)
 
 
 
-button_1.grid(row=3, column=0)
-button_2.grid(row=3, column=1)
-button_3.grid(row=3, column=2)
-button_add.grid(row = 3, column = 3)
+def addition():
+    global arithmetic
+    arithmetic = "+"
+    destroyMenu()
+    #2개의 번호생성 ex 9+5
+    x,y = click()
+    questionLabel = Label(frame,text="{0} + {1} = __".format(x,y),font = "Time 19")
+    questionLabel.grid(row=0,column=0,columnspan=2)
+    z = storeAns(x,y)
+    ansButton(z)
 
-button_clear.grid(row = 4, column = 3,columnspan = 2)
+def subtraction():
+    global arithmetic
+    arithmetic = "-"
+    destroyMenu()
+    #2개의 번호생성 ex 9+5
+    x,y = click()
+    questionLabel = Label(frame,text="{0} - {1} = __".format(x,y),font = "Time 19")
+    questionLabel.grid(row=0,column=0,columnspan=2)
+    z = storeAns(x,y)
+    ansButton(z)
 
-# button_backspace.grid(row=3, column=3,columnspan = 2)
+def multiplication():
+    global arithmetic
+    arithmetic = "x"
+    destroyMenu()
+    #2개의 번호생성 ex 9+5
+    x,y = click()
+    questionLabel = Label(frame,text="{0} x {1} = __".format(x,y),font = "Time 19")
+    questionLabel.grid(row=0,column=0,columnspan=2)
+    z = storeAns(x,y)
+    ansButton(z)
 
-button_4.grid(row=2, column=0)
-button_5.grid(row=2, column=1)
-button_6.grid(row=2, column=2)
-button_multiplication.grid(row=2, column=3,columnspan = 3)
+def dvision():
+    global arithmetic
+    arithmetic = "/"
+    destroyMenu()
+    #2개의 번호생성 ex 9+5
+    x,y = click()
+    questionLabel = Label(frame,text="{0} / {1} = __".format(x,y),font = "Time 19")
+    questionLabel.grid(row=0,column=0,columnspan=2)
+    z = storeAns(x,y)
+    ansButton(z)
 
-button_7.grid(row=1, column=0)
-button_8.grid(row=1, column=1)
-button_9.grid(row=1, column=2)
-button_subtraction.grid(row=1, column=3,columnspan = 3)
 
-button_0.grid(row=4, column=0)
-# button_add.grid(row = 5, column = 0)
-button_equal.grid(row = 4, column = 1,columnspan = 2)
 
+
+#buttons
+btn_addition = Button(frame,text="+", padx = 35, pady = 35, command = addition)
+btn_subtraction = Button(frame,text="-", padx = 35, pady = 35,command = subtraction)
+btn_multiplication = Button(frame,text="x", padx = 35, pady = 35,command = multiplication)
+btn_dvision = Button(frame,text="/", padx = 35, pady = 35, command = dvision)
+btn_exit = Button(frame,text = "Exit", padx = 35, pady = 35, command = win.quit)
+btn_exit.grid(row=2,column=0,columnspan=2)
+
+btn_addition.grid(row=0,column=0)
+btn_subtraction.grid(row=0,column=1)
+btn_multiplication.grid(row=1,column=0)
+btn_dvision.grid(row=1,column=1)
+
+frame.pack()
 
 
 win.mainloop()
+
+
